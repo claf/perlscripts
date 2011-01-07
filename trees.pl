67ab28e0308199e517e933add9ba1c72139874bb
Christophe Laferriere <christophe.laferriere@gmail.com>
2011-01-07 11:13:59 +0100

    Added tree random generation perl script

---
+++ trees.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $depth = $ARGV[0];
+my $work_min = $ARGV[1];
+my $work_max = $ARGV[2];
+my $degree_min = $ARGV[3];
+my $degree_max = $ARGV[4];
+
+die "use: tree.pl depth work_min work_max degree_min degree_max" unless defined $degree_max;
+
+my $current_node;
+my $last_node;
+my $current_depth = -1;
+
+my %father;
+my %depth;
+my %weight;
+my %children;
+
+while($current_depth < $depth) {
+	%father = ();
+	%depth = ();
+	%weight = ();
+	%children = ();
+	$last_node = 0;
+	$depth{0} = 0;
+	$weight{0} = int rand(1+$work_max - $work_min) + $work_min;
+
+	my @left_nodes = (0);
+	while(@left_nodes) {
+		$current_node = shift @left_nodes;
+		$current_depth = $depth{$current_node};
+		last if $current_depth == $depth;
+		my $children = int rand(1+$degree_max - $degree_min) + $degree_min;
+		my @children;
+		for(1..$children) {
+			$last_node++;
+			$father{$last_node} = $current_node;
+			$weight{$last_node} = int rand(1+$work_max - $work_min) + $work_min;
+			$depth{$last_node} = $current_depth + 1;
+			push @left_nodes, $last_node;
+			push @children, $last_node;
+		}
+		$children{$current_node} = [ @children ];
+	}
+}
+
+for(0..$last_node) {
+	print "$_ $weight{$_} / ";
+    print join(' ', @{$children{$_}}) if defined $children{$_};
+	print "\n";
+}
+
+
+
